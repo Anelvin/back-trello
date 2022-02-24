4b926683b81d094c62fb3a105e9265831426b6cc
Anelvin <amejiaroque@gmail.com>
2022-02-24 23:59:50 +0100

    Create migrations

--- app.js
+++ app.js
@@ -2,12 +2,27 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hola Mundo')
+import userRoutes from './routes/userRoutes.js';
+
+app.set('port', process.env.PORT || 3001);
+
+//Middleware
+app.use( function ( req, res, next ) {
+    res.header( "Access-Control-Allow-Origin", "*" );
+    res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+    res.header( "Access-Control-Allow-Methods", "POST, PUT, PATCH, DELETE, GET" );
+    next();
 });
 
-const PORT = 3001;
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-app.listen(PORT, () => {
-    console.log('Server on port ' + PORT);
+app.use('/users', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hola Mundo');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 })
